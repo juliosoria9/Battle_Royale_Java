3131638fe07bdc4f5a7e556bfba79d40b0745d47
trudytru30 <157128336+trudytru30@users.noreply.github.com>
2024-12-09 15:49:28 +0100

    creacion de enemigos

--- src/personajes/Enemigo.java
+++ src/personajes/Enemigo.java
@@ -4,11 +4,8 @@ import Herramienta.Herramienta;
 
 public class Enemigo extends Personaje{
 	
-	private String dificultad;
-
-	public Enemigo(int vida,  Herramienta arma, String nombre, String dificultad)
+	public Enemigo(int vida, Herramienta arma, String nombre)
 	{
 		super(vida, arma, nombre);
-		this.dificultad = dificultad;
 	}
 }
