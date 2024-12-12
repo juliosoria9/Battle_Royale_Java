6d281a4f2f012b8a99966d65fe8d5eae31e812fb
juliosoria9 <162936156+juliosoria9@users.noreply.github.com>
2024-12-12 18:22:27 +0100

    cambios

--- src/partida/Tablero.java
+++ src/partida/Tablero.java
@@ -6,6 +6,34 @@ public class Tablero {
 	private int[][] tablero;
     private int[] personajes;
 
+    //inicializar la partida
+    // el tamaño del tablero tiene que ser 9 veces mas grandes que el de los jugadores de forma
+    public void meterPersonajes() {
+        int fila = 0;
+        int columna = 0;
+
+        for (int i = 0; i < personajes.length; i++) {
+            personajes[i].setX(fila);
+            personajes[i].setY(columna);
+
+           
+            if (fila + 3 < tablero.length) { 
+                fila += 3; 
+            } else {
+
+                fila = 0;
+                if (columna + 3 < tablero[0].length) { 
+                    columna = columna +3; 
+                } else {
+  
+                    return;
+                }
+            }
+        }
+    }
+
+    
+    
     // construir de una cantidad especifica de filas y columna
     public Tablero(int filas, int columnas,int []array_jugadores) {
         tablero = new int[filas][columnas];
@@ -14,10 +42,10 @@ public class Tablero {
 
     // cambiar valor en una posicion especifica
     public void asignarValor(int fila, int columna, int valor) {
-        if (fila >= 0 && fila < tablero.length && columna >= 0 && columna < tablero[0].length) {
+        if (fila >= 0 && fila < tablero.length && columna >= 0 && columna < tablero[0].length) { //comprar que este dentro del rango
             tablero[fila][columna] = valor;
         } else {
-            System.out.println("Indice fuera de rango.");
+            System.out.println("indice fuera de rango.");
         }
     }
 
@@ -26,7 +54,7 @@ public class Tablero {
         if (fila >= 0 && fila < tablero.length && columna >= 0 && columna < tablero[0].length) {
             return tablero[fila][columna];
         } else {
-            System.out.println("Índice fuera de rango.");
+            System.out.println("indice fuera de rango.");
             return -1; //si hay error devuelve -1
         }
     }
